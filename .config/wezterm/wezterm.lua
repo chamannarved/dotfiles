eade3e390b069887ac7fe9a237d45245ecc889c6
Chaman Lal Narved <chamannarved@gmail.com>
2024-10-04 20:30:08 +0530

    Add wezterm config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,24 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+-- Set PowerShell as default shell
+config.default_prog = { "C:/Windows/System32/WindowsPowerShell/v1.0/powershell.exe" }
+
+config.window_padding = {
+	left = 3,
+	right = 3,
+	top = 3,
+	bottom = 0,
+}
+config.window_decorations = "RESIZE"
+
+-- For example, changing the color scheme:
+config.color_scheme = "tokyonight_night"
+config.font = wezterm.font("JetBrains Mono")
+
+-- and finally, return the configuration to wezterm
+return config
