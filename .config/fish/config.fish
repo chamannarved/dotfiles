77c8a565911a0fc301a9ece2c981a6718f8c6b0b
Chaman Narved <chamannarved@gmail.com>
2024-11-20 14:01:24 +0530

    Add alias for nvim

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,7 @@
 set fish_greeting ""
 
 alias config='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
+alias vim=nvim
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
