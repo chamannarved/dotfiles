2eaddbd3049aaf4dfade4ce1072acd03f1884e87
Chaman Lal Narved <chamannarved@gmail.com>
2025-05-30 00:49:03 +0530

    Refector new nvim config

--- .config/nvim/lua/config/keybinds.lua
+++ .config/nvim/lua/config/keybinds.lua
@@ -5,23 +5,23 @@ vim.g.mapleader = " "
 keymap.set("n", "<leader>e", vim.cmd.Ex)
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
-keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 -- Diagnostic keymaps
-keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 -- Lazy
-keymap.set('n', '<leader>l', vim.cmd.Lazy, { desc = 'Open [L]azy UI' })
+keymap.set("n", "<leader>l", vim.cmd.Lazy, { desc = "Open [L]azy UI" })
 -- UndoTree
-keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, { desc = 'Toggle [U]ndoTree Panel' })
+keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Toggle [U]ndoTree Panel" })
 -- Move Lines
-keymap.set('n', '<A-j>', '<cmd>m .+1<cr>==', { desc = 'Move Down' })
-keymap.set('n', '<A-k>', '<cmd>m .-2<cr>==', { desc = 'Move Up' })
-keymap.set('i', '<A-j>', '<esc><cmd>m .+1<cr>==gi', { desc = 'Move Down' })
-keymap.set('i', '<A-k>', '<esc><cmd>m .-2<cr>==gi', { desc = 'Move Up' })
-keymap.set('v', '<A-j>', ":m '>+1<cr>gv=gv", { desc = 'Move Down' })
-keymap.set('v', '<A-k>', ":m '<-2<cr>gv=gv", { desc = 'Move Up' })
+keymap.set("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move Down" })
+keymap.set("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move Up" })
+keymap.set("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
+keymap.set("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
+keymap.set("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move Down" })
+keymap.set("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move Up" })
 
 -- Live Server
-keymap.set('n', '<C-l>', '<cmd>LiveServer<cr>', { desc = 'Start/Stop [L]ive Server' })
+keymap.set("n", "<C-l>", "<cmd>LiveServer<cr>", { desc = "Start/Stop [L]ive Server" })
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -3,35 +3,28 @@ return {
     { "folke/todo-comments.nvim", opts = {} },
 
     { -- This helps with php/html for indentation
-        'captbaritone/better-indent-support-for-php-with-html',
+        "captbaritone/better-indent-support-for-php-with-html",
     },
 
     { -- This helps with ssh tunneling and copying to clipboard
-        'ojroques/vim-oscyank',
+        "ojroques/vim-oscyank",
     },
 
     { -- Git plugin
-        'tpope/vim-fugitive',
+        "tpope/vim-fugitive",
     },
 
     { -- Show historical versions of the file locally
-        'mbbill/undotree',
+        "mbbill/undotree",
     },
 
     { -- Show CSS Colors
-        'brenoprata10/nvim-highlight-colors',
+        "brenoprata10/nvim-highlight-colors",
         config = function()
-            require('nvim-highlight-colors').setup({})
-        end
-    },
-    {
-        "aurum77/live-server.nvim",
-        run = function()
-            require "live_server.util".install()
+            require("nvim-highlight-colors").setup({})
         end,
-        cmd = { "LiveServer", "LiveServerStart", "LiveServerStop" },
     },
     {
-        'mbbill/undotree',
+        "mbbill/undotree",
     },
 }
