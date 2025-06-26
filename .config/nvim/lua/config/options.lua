1af63d87d11f017795624edba43bd109f95dd7dd
Chaman Lal Narved <chamannarved@gmail.com>
2025-06-26 19:26:23 +0530

    Fix UndoTree not working in windows

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -3,7 +3,7 @@ vim.opt.cursorline = true
 vim.opt.relativenumber = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
-vim.opt.clipboard = vim.env.SSH_TTY and "" or "unnamedplus"
+vim.opt.clipboard = vim.env.SSH_TTY and '' or 'unnamedplus'
 vim.opt.expandtab = true
 vim.opt.scrolloff = 4
 vim.opt.undofile = true
@@ -16,3 +16,9 @@ vim.api.nvim_create_autocmd('TextYankPost', {
         vim.hl.on_yank()
     end,
 })
+
+-- FC is supposed to be the windows' diff equivalent
+local os_info = vim.loop.os_uname()
+if os_info.sysname == 'Windows_NT' then
+    vim.g.undotree_DiffCommand = 'FC'
+end
