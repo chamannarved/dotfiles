13ca6b8656bac75f95618966420d0b6c3d0fd3e6
Chaman Narved <chamannarved@gmail.com>
2025-08-13 17:04:31 +0530

    fix: highlight on yank autocmd

--- .config/nvim/lua/chamannarved/opts.lua
+++ .config/nvim/lua/chamannarved/opts.lua
@@ -11,9 +11,8 @@ vim.opt.undofile = true
 -- Highlight when yanking (copying) text
 vim.api.nvim_create_autocmd('TextYankPost', {
     desc = 'Highlight when yanking (copying) text',
-    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
     callback = function()
-        vim.hl.on_yank()
+        vim.highlight.on_yank()
     end,
 })
 
