602c07d8d9f4c67a0ed9b773bc96f36d5455966c
Chaman Narved <chamannarved@gmail.com>
2025-08-13 16:02:45 +0530

    fix: set mason to v1.x because mason v2.x use new nvim API

--- .config/nvim/lua/chamannarved/plugins/lsp.lua
+++ .config/nvim/lua/chamannarved/plugins/lsp.lua
@@ -1,25 +1,37 @@
+local nvim_version = vim.version()
+local mason_lspconfig_version
+
+if nvim_version.major == 0 and nvim_version.minor < 11 then
+    mason_lspconfig_version = 'v1.x'
+else
+    mason_lspconfig_version = 'v2.x'
+end
+
 return {
-    "neovim/nvim-lspconfig",
+    'neovim/nvim-lspconfig',
     dependencies = {
         {
             -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
             -- used for completion, annotations and signatures of Neovim apis
-            "folke/lazydev.nvim",
-            ft = "lua",
+            'folke/lazydev.nvim',
+            ft = 'lua',
             opts = {
                 library = {
                     -- Load luvit types when the `vim.uv` word is found
-                    { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+                    { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
                 },
             },
         },
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
+        'williamboman/mason.nvim',
+        {
+            'williamboman/mason-lspconfig.nvim',
+            version = mason_lspconfig_version,
+        },
 
-        "WhoIsSethDaniel/mason-tool-installer.nvim",
+        'WhoIsSethDaniel/mason-tool-installer.nvim',
         -- Useful status updates for LSP.
         {
-            "j-hui/fidget.nvim",
+            'j-hui/fidget.nvim',
             opts = {
                 notification = {
                     window = {
@@ -30,29 +42,29 @@ return {
             },
         },
         -- Autocompletion
-        "hrsh7th/nvim-cmp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "saadparwaiz1/cmp_luasnip",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-nvim-lua",
+        'hrsh7th/nvim-cmp',
+        'hrsh7th/cmp-buffer',
+        'hrsh7th/cmp-path',
+        'saadparwaiz1/cmp_luasnip',
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-nvim-lua',
         -- Snippets
-        "L3MON4D3/LuaSnip",
-        "rafamadriz/friendly-snippets",
+        'L3MON4D3/LuaSnip',
+        'rafamadriz/friendly-snippets',
         -- Allows extra capabilities provided by blink.cmp
-        "saghen/blink.cmp",
+        'saghen/blink.cmp',
         { -- Autoformat
-            "stevearc/conform.nvim",
-            event = { "BufWritePre" },
-            cmd = { "ConformInfo" },
+            'stevearc/conform.nvim',
+            event = { 'BufWritePre' },
+            cmd = { 'ConformInfo' },
             keys = {
                 {
-                    "<leader>f",
+                    '<leader>f',
                     function()
-                        require("conform").format({ async = true, lsp_format = "fallback" })
+                        require('conform').format { async = true, lsp_format = 'fallback' }
                     end,
-                    mode = "",
-                    desc = "[F]ormat buffer",
+                    mode = '',
+                    desc = '[F]ormat buffer',
                 },
             },
             opts = {
@@ -67,12 +79,12 @@ return {
                     else
                         return {
                             timeout_ms = 500,
-                            lsp_format = "fallback",
+                            lsp_format = 'fallback',
                         }
                     end
                 end,
                 formatters_by_ft = {
-                    lua = { "stylua" },
+                    lua = { 'stylua' },
                     -- Conform can also run multiple formatters sequentially
                     -- python = { "isort", "black" },
                     --
@@ -84,24 +96,24 @@ return {
     },
     config = function()
         local autoformat_filetypes = {
-            "lua",
+            'lua',
         }
         -- Create a keymap for vim.lsp.buf.implementation
-        vim.api.nvim_create_autocmd("LspAttach", {
+        vim.api.nvim_create_autocmd('LspAttach', {
             callback = function(args)
                 local client = vim.lsp.get_client_by_id(args.data.client_id)
                 if not client then
                     return
                 end
                 if vim.tbl_contains(autoformat_filetypes, vim.bo.filetype) then
-                    vim.api.nvim_create_autocmd("BufWritePre", {
+                    vim.api.nvim_create_autocmd('BufWritePre', {
                         buffer = args.buf,
                         callback = function()
-                            vim.lsp.buf.format({
+                            vim.lsp.buf.format {
                                 formatting_options = { tabSize = 4, insertSpaces = true },
                                 bufnr = args.buf,
                                 id = client.id,
-                            })
+                            }
                         end,
                     })
                 end
@@ -109,137 +121,134 @@ return {
         })
 
         -- Add borders to floating windows
-        vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
-        vim.lsp.handlers["textDocument/signatureHelp"] =
-            vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
+        vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, { border = 'rounded' })
+        vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help,
+            { border = 'rounded' })
 
         -- Configure error/warnings interface
-        vim.diagnostic.config({
+        vim.diagnostic.config {
             virtual_text = true,
             severity_sort = true,
             float = {
-                style = "minimal",
-                border = "rounded",
-                header = "",
-                prefix = "",
+                style = 'minimal',
+                border = 'rounded',
+                header = '',
+                prefix = '',
             },
             signs = false,
-        })
+        }
 
         -- Add cmp_nvim_lsp capabilities settings to lspconfig
         -- This should be executed before you configure any language server
-        local lspconfig_defaults = require("lspconfig").util.default_config
-        lspconfig_defaults.capabilities = vim.tbl_deep_extend(
-            "force",
-            lspconfig_defaults.capabilities,
-            require("cmp_nvim_lsp").default_capabilities()
-        )
+        local lspconfig_defaults = require('lspconfig').util.default_config
+        lspconfig_defaults.capabilities = vim.tbl_deep_extend('force', lspconfig_defaults.capabilities,
+            require('cmp_nvim_lsp').default_capabilities())
 
         -- This is where you enable features that only work
         -- if there is a language server active in the file
-        vim.api.nvim_create_autocmd("LspAttach", {
+        vim.api.nvim_create_autocmd('LspAttach', {
             callback = function(event)
                 local opts = { buffer = event.buf }
 
-                vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
-                vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
-                vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
-                vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
-                vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
-                vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
-                vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
-                vim.keymap.set("n", "gl", "<cmd>lua vim.diagnostic.open_float()<cr>", opts)
-                vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
-                vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
-                vim.keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+                vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+                vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+                vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+                vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+                vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+                vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+                vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+                vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
+                vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+                vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+                vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
             end,
         })
 
-        require("mason").setup({})
-        require("mason-lspconfig").setup({
+        require('mason').setup {}
+        require('mason-lspconfig').setup {
             ensure_installed = {
-                "lua_ls",
-                "intelephense",
-                "ts_ls",
-                "eslint",
-                "html",
-                "cssls",
+                'lua_ls',
+                'intelephense',
+                'ts_ls',
+                'eslint',
+                'html',
+                'cssls',
             },
             handlers = {
                 -- this first function is the "default handler"
                 -- it applies to every language server without a custom handler
                 function(server_name)
-                    require("lspconfig")[server_name].setup({})
+                    require('lspconfig')[server_name].setup {}
                 end,
 
                 -- this is the "custom handler" for `lua_ls`
                 lua_ls = function()
-                    require("lspconfig").lua_ls.setup({
+                    require('lspconfig').lua_ls.setup {
                         settings = {
                             Lua = {
                                 runtime = {
-                                    version = "LuaJIT",
+                                    version = 'LuaJIT',
                                 },
                                 diagnostics = {
-                                    globals = { "vim" },
+                                    globals = { 'vim' },
                                 },
                                 workspace = {
                                     library = { vim.env.VIMRUNTIME },
                                 },
                             },
                         },
-                    })
+                    }
                 end,
             },
-        })
+        }
 
-        local cmp = require("cmp")
+        local cmp = require 'cmp'
 
-        require("luasnip.loaders.from_vscode").lazy_load()
+        require('luasnip.loaders.from_vscode').lazy_load()
 
-        vim.opt.completeopt = { "menu", "menuone", "noselect" }
+        vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
 
-        cmp.setup({
-            preselect = "item",
+        cmp.setup {
+            preselect = 'item',
             completion = {
-                completeopt = "menu,menuone,noinsert",
+                completeopt = 'menu,menuone,noinsert',
             },
             window = {
                 documentation = cmp.config.window.bordered(),
             },
             sources = {
-                { name = "path" },
-                { name = "nvim_lsp" },
-                { name = "buffer",  keyword_length = 3 },
-                { name = "luasnip", keyword_length = 2 },
+                { name = 'path' },
+                { name = 'nvim_lsp' },
+                { name = 'buffer',  keyword_length = 3 },
+                { name = 'luasnip', keyword_length = 2 },
             },
             snippet = {
                 expand = function(args)
-                    require("luasnip").lsp_expand(args.body)
+                    require('luasnip').lsp_expand(args.body)
                 end,
             },
             formatting = {
-                fields = { "abbr", "menu", "kind" },
+                fields = { 'abbr', 'menu', 'kind' },
                 format = function(entry, item)
                     local n = entry.source.name
-                    if n == "nvim_lsp" then
-                        item.menu = "[LSP]"
+                    if n == 'nvim_lsp' then
+                        item.menu = '[LSP]'
                     else
-                        item.menu = string.format("[%s]", n)
+                        item.menu = string.format('[%s]', n)
                     end
                     return item
                 end,
             },
-            mapping = cmp.mapping.preset.insert({
+            mapping = cmp.mapping.preset.insert {
                 -- confirm completion item
-                ["<CR>"] = cmp.mapping.confirm({ select = false }),
+                ['<CR>'] = cmp.mapping.confirm { select = false },
 
                 -- scroll documentation window
-                ["<C-f>"] = cmp.mapping.scroll_docs(5),
-                ["<C-u>"] = cmp.mapping.scroll_docs(-5),
+                ['<C-f>'] = cmp.mapping.scroll_docs(5),
+                ['<C-u>'] = cmp.mapping.scroll_docs(-5),
 
                 -- toggle completion menu
-                ["<C-e>"] = cmp.mapping(function(fallback)
+                ['<C-e>'] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         cmp.abort()
                     else
@@ -248,43 +257,43 @@ return {
                 end),
 
                 -- tab complete
-                ["<Tab>"] = cmp.mapping(function(fallback)
-                    local col = vim.fn.col(".") - 1
+                ['<Tab>'] = cmp.mapping(function(fallback)
+                    local col = vim.fn.col '.' - 1
 
                     if cmp.visible() then
-                        cmp.select_next_item({ behavior = "select" })
-                    elseif col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
+                        cmp.select_next_item { behavior = 'select' }
+                    elseif col == 0 or vim.fn.getline('.'):sub(col, col):match '%s' then
                         fallback()
                     else
                         cmp.complete()
                     end
-                end, { "i", "s" }),
+                end, { 'i', 's' }),
 
                 -- go to previous item
-                ["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = "select" }),
+                ['<S-Tab>'] = cmp.mapping.select_prev_item { behavior = 'select' },
 
                 -- navigate to next snippet placeholder
-                ["<C-d>"] = cmp.mapping(function(fallback)
-                    local luasnip = require("luasnip")
+                ['<C-d>'] = cmp.mapping(function(fallback)
+                    local luasnip = require 'luasnip'
 
                     if luasnip.jumpable(1) then
                         luasnip.jump(1)
                     else
                         fallback()
                     end
-                end, { "i", "s" }),
+                end, { 'i', 's' }),
 
                 -- navigate to the previous snippet placeholder
-                ["<C-b>"] = cmp.mapping(function(fallback)
-                    local luasnip = require("luasnip")
+                ['<C-b>'] = cmp.mapping(function(fallback)
+                    local luasnip = require 'luasnip'
 
                     if luasnip.jumpable(-1) then
                         luasnip.jump(-1)
                     else
                         fallback()
                     end
-                end, { "i", "s" }),
-            }),
-        })
+                end, { 'i', 's' }),
+            },
+        }
     end,
 }
