1e1afee373f3176761187847aac310ffeb22ac4f
Chaman Narved <chamannarved@gmail.com>
2025-10-12 13:58:25 +0530

    fix:nvim theme change and lualine config also some code formating config

--- .config/nvim/lua/chamannarved/opts.lua
+++ .config/nvim/lua/chamannarved/opts.lua
@@ -1,8 +1,8 @@
 vim.opt.number = true
 vim.opt.cursorline = true
 vim.opt.relativenumber = true
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
 vim.opt.clipboard = vim.env.SSH_TTY and '' or 'unnamedplus'
 vim.opt.expandtab = true
 vim.opt.scrolloff = 4
@@ -10,14 +10,14 @@ vim.opt.undofile = true
 
 -- Highlight when yanking (copying) text
 vim.api.nvim_create_autocmd('TextYankPost', {
-    desc = 'Highlight when yanking (copying) text',
-    callback = function()
-        vim.highlight.on_yank()
-    end,
+  desc = 'Highlight when yanking (copying) text',
+  callback = function()
+    vim.highlight.on_yank()
+  end,
 })
 
 -- FC is supposed to be the windows' diff equivalent
 local os_info = vim.loop.os_uname()
 if os_info.sysname == 'Windows_NT' then
-    vim.g.undotree_DiffCommand = 'FC'
+  vim.g.undotree_DiffCommand = 'FC'
 end
--- .config/nvim/lua/chamannarved/plugins/colors.lua
+++ .config/nvim/lua/chamannarved/plugins/colors.lua
@@ -2,20 +2,19 @@
 --     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 -- end
 return {
+    -- {
+    --     "folke/tokyonight.nvim",
+    --     config = function()
+    --         vim.cmd.colorscheme("tokyonight")
+    --         -- enable_transparency()
+    --     end,
+    -- },
     {
-        "folke/tokyonight.nvim",
+        'ellisonleao/gruvbox.nvim',
+        priority = 1000,
         config = function()
-            vim.cmd.colorscheme("tokyonight")
-            -- enable_transparency()
+            vim.cmd.colorscheme 'gruvbox'
         end,
-    },
-    {
-        "nvim-lualine/lualine.nvim",
-        dependencies = {
-            "nvim-tree/nvim-web-devicons",
-        },
-        opts = {
-            theme = "tokyonight",
-        },
+        opts = ...,
     },
 }
