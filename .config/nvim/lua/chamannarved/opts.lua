0b4663c4efabbd089f8d953276f0dfd3479ebaa3
Chaman Narved <chamannarved@gmail.com>
2026-05-23 12:22:52 +0530

    Update options

--- .config/nvim/lua/chamannarved/opts.lua
+++ .config/nvim/lua/chamannarved/opts.lua
@@ -1,12 +1,40 @@
+vim.g.netrw_banner = 0
+
 vim.opt.number = true
-vim.opt.cursorline = true
 vim.opt.relativenumber = true
-vim.opt.shiftwidth = 2
+
+vim.opt.cursorline = true
+
 vim.opt.tabstop = 2
-vim.opt.clipboard = vim.env.SSH_TTY and '' or 'unnamedplus'
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
 vim.opt.expandtab = true
+
+vim.opt.wrap = false
+vim.opt.smartindent = true
+vim.opt.inccommand = 'split'
+
+vim.opt.clipboard = vim.env.SSH_TTY and '' or 'unnamedplus'
+
 vim.opt.scrolloff = 4
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.laststatus = 3
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = vim.fn.stdpath 'data' .. '/undodir'
 vim.opt.undofile = true
+
+vim.opt.completeopt = 'menuone,noselect,fuzzy,nosort'
+vim.opt.shortmess:append 'c'
+vim.opt.isfname:append '@-@'
+
+vim.opt.signcolumn = 'yes'
 
 -- Highlight when yanking (copying) text
 vim.api.nvim_create_autocmd('TextYankPost', {
